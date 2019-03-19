501726717a5def843660d0ead03d09f21d523f31
Thomas Phillips <tsphillips@users.noreply.github.com>
2019-03-19 19:51:47 -0400

    Create Monsters.js

---
+++ Monsters.js
@@ -0,0 +1,21 @@
+(function(){
+  var monsters = {
+    { name : 'dragon',
+      stats : {
+        hp : 42,
+        str : 42,
+        size : 42
+      }
+    },
+    { name : 'squirrel',
+      stats : {
+        hp : 2,
+        str : 2,
+        size : 2
+      }
+    }
+  }; // monsters
+  
+  window.TEST = window.TEST || {};
+  window.TEST.monsters = monsters;
+)();
