ceadf1238a607507790f9167c38354fa11805db5
Thomas Phillips <tsphillips@users.noreply.github.com>
2019-03-19 19:53:27 -0400

    Update Monsters.js

--- Monsters.js
+++ Monsters.js
@@ -1,6 +1,6 @@
 (function(){
   var monsters = {
-    { name : 'dragon',
+    'dragon' : { name : 'dragon',
       stats : {
         hp : 42,
         str : 42,
@@ -13,7 +13,9 @@
         str : 2,
         size : 2
       }
-    }
+    },
+ { name : 'cat',
+  garble garble.....
   }; // monsters
   
   window.TEST = window.TEST || {};
